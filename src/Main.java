60396d7c2db6ba37364bd8e53e2071bb595cabe3
mnkjp <menkejp@mail.uc.edu>
2024-12-13 16:51:12 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+        double salesTax = .05;
+        int price = 0;
+        int intOperandA = 1;
+        int intOperandB = 2;
+        int sum = intOperandA + intOperandB;
+        int product = intOperandA * intOperandB;
+        int difference = sum - product;
+        int quotient = sum / sum;
+        int modulo = sum % sum;
+        double operandC = 3.5;
+        double operandD = 4.5;
+        double sumA = operandC + operandD;
+        double productA = operandC * operandD;
+        double quotientA = operandC / operandD;
+        double moduloA = operandC % operandD;
+
+        int numberOfKids = 0;
+        boolean isItRaining = true;
+        double gasPrice = 3.1;
+        int myFavNum = 23;
+        double shoeSize = 25;
+        int birthMonth = 12;
+        String myName = "John Menke";
+
+
+        System.out.println("The sum of operand a + operand b is " + sum);
+        System.out.println("The product of operand a + operand b is " + product);
+        System.out.println("The quotient of operand a + operand b is " + quotient);
+        System.out.println("The modulo of operand a + operand b is " + modulo);
+        System.out.println("The sum of operand C and operand D is " + sumA);
+        System.out.println("The product of operand C and operand D is " + productA);
+        System.out.println("The quotient of operand C and operand D is " + quotientA);
+        System.out.println("The modulo of operand C and operand D is " + moduloA);
+
+
+    }
+
+}
